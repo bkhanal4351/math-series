f0b24949ec198b749286bed6f629de589ba01e5f
Bishal Khanal <bishal.khanal87@gmail.com>
2022-04-20 23:13:13 -0700

    fix readme and doc-string

--- math_series/series.py
+++ math_series/series.py
@@ -1,19 +1,18 @@
-"""
-The function has one parameter n. The function returns the nth value in the fibonacci series.
-"""
+
 def fibonacci(n):
+    """
+    The function has one parameter n. The function returns the nth value in the fibonacci series.
+    """
     if n in {0, 1}:
         return n
 
     return fibonacci(n-1) + fibonacci(n-2)
 
 
-"""
-This function returns the nth value in the lucas numbers
-"""
-
-
 def lucas(n):
+    """
+    This function returns the nth value in the lucas numbers
+    """
     if n == 0:
         return 2
     elif n == 1:
@@ -21,14 +20,12 @@ def lucas(n):
     return lucas(n-1) + lucas(n-2)
 
 
-"""
-This function has one required parameter and two optional parameter.
-Calling it with the optional arguments 2 and 1 will produce values from the lucas numbers.
-Other values for the optional parameters will produce other series.
-"""
-
-
 def sum_series(n, first_num=0, second_num=1):
+    """
+    This function has one required parameter and two optional parameter.
+    Calling it with the optional arguments 2 and 1 will produce values from the lucas numbers.
+    Other values for the optional parameters will produce other series.
+    """
     if n == 0:
         return first_num
     elif n == 1:
